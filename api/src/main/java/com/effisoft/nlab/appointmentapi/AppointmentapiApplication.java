b6ec72cf314868ce772bdb16c721d5105f46e097
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2025-01-30 14:10:12 -0800

    Initial api project

---
+++ api/src/main/java/com/effisoft/nlab/appointmentapi/AppointmentapiApplication.java
@@ -0,0 +1,13 @@
+package com.effisoft.nlab.appointmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentapiApplication.class, args);
+	}
+
+}
