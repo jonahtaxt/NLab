9800ae53aeb992a1f27c8bc836d1014cd1b7281a
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2025-02-20 09:12:22 -0800

    Update Payment Method entity

--- DatabaseProjectNlab/dbo/Tables/PaymentMethod.sql
+++ DatabaseProjectNlab/dbo/Tables/PaymentMethod.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[PaymentMethod] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (50)  NOT NULL,
-    [Description] NVARCHAR (200) NULL,
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50)  NOT NULL,
+    [Description]  NVARCHAR (200) NULL,
+    [DisplayOrder] INT DEFAULT 0  NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
