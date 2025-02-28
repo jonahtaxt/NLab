8329aa116d87d8addac0d5ad02bf2ba73ad6e3e1
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2025-02-28 12:35:22 -0800

    NLab style applied

--- web/app/ui/dashboard/nav-links.tsx
+++ web/app/ui/dashboard/nav-links.tsx
@@ -3,25 +3,37 @@
 import {
   UserGroupIcon,
   HomeIcon,
-  UserIcon
+  UserIcon,
+  CubeIcon
 } from '@heroicons/react/24/outline';
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { CogIcon } from 'lucide-react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
+const links = [{ 
+    name: 'Inicio',
+    href: '/dashboard',
+    icon: HomeIcon
+  }, {
     name: 'Nutriólogos',
     href: '/dashboard/nutritionists',
     icon: UserIcon,
-  },
-  { name: 'Pacientes', 
+  }, { name: 'Pacientes', 
     href: '/dashboard/patients', 
-    icon: UserGroupIcon },
+    icon: UserGroupIcon 
+  }, {
+    name: 'Paquetes',
+    href: '/dashboard/packages',
+    icon: CubeIcon,
+  }, {
+    name: 'Configuración',
+    href: '/dashboard/settings',
+    icon: CogIcon,
+  }, 
 ];
 
 export default function NavLinks() {
@@ -35,9 +47,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-nlab-coral hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
             {
-              'bg-sky-100 text-blue-600': pathname == link.href
+              'bg-nlab-coral text-white': pathname == link.href
             },
             )}
           >
--- web/app/ui/dashboard/sidenav.tsx
+++ web/app/ui/dashboard/sidenav.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { logout } from '@/app/lib/auth';
+import Image from 'next/image';
 
 const handleLogoff = () => {
   logout();
@@ -13,9 +14,9 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-center rounded-md bg-nlab-coral p-3 md:h-40"
         href="/"
-      >
+      ><Image src="/logo.png" alt="Logo" width={135} height={135} />
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
@@ -23,7 +24,7 @@ export default function SideNav() {
         <form>
           <button
             onClick={handleLogoff}
-            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-nlab-coral hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
