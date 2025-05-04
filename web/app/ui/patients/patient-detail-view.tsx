6791ef24c0cb83415ff27650cd4c775f3206be13
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2025-05-04 09:16:39 -0700

    Move patient personal data to control

--- web/app/ui/patients/patient-detail-view.tsx
+++ web/app/ui/patients/patient-detail-view.tsx
@@ -2,7 +2,6 @@
 
 import { PackageTypeSelectDTO, Patient, PaginatedResponse, PurchasedPackage, PurchasedPackageDTO, PatientAppointmentView } from "@/app/lib/definitions";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useEffect, useState } from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -16,7 +15,8 @@ import PatientPaymentForm from "@/app/ui/patients/patient-payment-form";
 import PatientPackageDetail from "@/app/ui/patients/patient-package-detail";
 import AppointmentForm from "@/app/ui/appointments/appointment-form";
 import PatientAppointments from "@/app/ui/patients/patient-appointments";
-import AppointmentNotes from "../appointments/appointment-notes";
+import AppointmentNotes from "@/app/ui/appointments/appointment-notes";
+import PatientPersonalData from "@/app/ui/patients/patient-personal-data";
 
 interface PatientDetailViewProps {
   patient: Patient;
@@ -244,61 +244,7 @@ const PatientDetailView = ({ patient, onBack }: PatientDetailViewProps) => {
         <div className="flex flex-col gap-4 p-4">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
             <div className="md:col-span-1">
-              <Card>
-                <CardHeader className="bg-gray-50 rounded-t-xl">
-                  <CardTitle className="flex items-center gap-2 text-nlab-black">
-                    Información Personal
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="p-6">
-                  <div className="mb-4">
-                    <p className="text-sm text-gray-500">Nombre Completo</p>
-                    <p className="font-medium">{patient.firstName} {patient.lastName}</p>
-                  </div>
-                  <div className="mb-4">
-                    <p className="text-sm text-gray-500">Correo Electrónico</p>
-                    <p className="font-medium">{patient.email}</p>
-                  </div>
-                  <div className="mb-4">
-                    <p className="text-sm text-gray-500">Teléfono</p>
-                    <p className="font-medium">{patient.phone}</p>
-                  </div>
-                  <div className="mb-4">
-                    <p className="text-sm text-gray-500">Estado</p>
-                    <span className={`inline-flex items-center rounded-full px-2 py-1 text-xs font-medium ${patient.active ? 'bg-green-100 text-green-700' : 'bg-gray-100 text-gray-700'
-                      }`}>
-                      {patient.active ? 'Activo' : 'Inactivo'}
-                    </span>
-                  </div>
-                  <div className="mb-4">
-                    <p className="text-sm text-gray-500">Fecha de Creación</p>
-                    <p className="font-medium">
-                      {new Date(patient.createdAt).toLocaleDateString('es-MX', {
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric'
-                      })}
-                    </p>
-                  </div>
-                  {patient.updatedAt && (
-                    <div className="mb-4">
-                      <p className="text-sm text-gray-500">Última Actualización</p>
-                      <p className="font-medium">
-                        {new Date(patient.updatedAt).toLocaleDateString('es-MX', {
-                          day: '2-digit',
-                          month: '2-digit',
-                          year: 'numeric'
-                        })}
-                      </p>
-                    </div>
-                  )}
-                </CardContent>
-                <CardFooter className="flex flex-col sm:flex-row gap-2 justify-between p-6">
-                  <Button
-                    onClick={handleAddAppointment}
-                    className="w-full">Agendar Cita</Button>
-                </CardFooter>
-              </Card>
+              <PatientPersonalData patient={patient} handleAddAppointment={handleAddAppointment} />
             </div>
 
             <div className="md:col-span-3">
